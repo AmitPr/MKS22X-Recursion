a694961790bdf08b8f8f27474fa326c4a3402978
Amit Prasad <17amitprasad@gmail.com>
2019-01-31 18:58:43 -0500

    Initial Commit + SQRT function added

---
+++ recursion.java
@@ -0,0 +1,34 @@
+public class recursion{
+    /*You may write additional private methods */
+
+    /*Recursively find the sqrt using Newton's approximation
+     *tolerance is the allowed percent error the squared answer is away from n.
+     *precondition: n is non-negative
+
+    */
+	public static double sqrt(double n, double tolerance){
+		if (n == 0)
+			return 0;
+		return sqrtHelper (n, 1, tolerance);
+	}
+	public static double sqrtHelper(double n, double current, double tolerance){
+		current = ( (n / current) + current) / 2;
+		if (Math.abs(((current * current) - n)) < tolerance)
+			return current;
+		else
+			return sqrtHelper(n, current, tolerance);
+	}
+
+    /*Recursively find the n'th fibbonaci number in linear time
+     *fib(0) = 1; fib(1) = 1; fib(5) = 5
+     *precondition: n is non-negative
+     */
+    public static int fib(int n){
+
+    }
+
+    /*As Per classwork*/
+    public static ArrayList<Integer> makeAllSums(){
+    }
+
+}
